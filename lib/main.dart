1b1a3ffbe9bec146e4155d8249b98b95a8a49015
Zyttal <calz483k@gmail.com>
2024-06-28 20:03:22 +0800

    Add Firebase Dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:state_change_demo/firebase_options.dart';
 import 'package:state_change_demo/src/controllers/auth_controller.dart';
 import 'package:state_change_demo/src/routing/router.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseFirestore.instance.settings =
+      const Settings(persistenceEnabled: true);
   AuthController.initialize();
   GlobalRouter.initialize();
   await AuthController.I.loadSession();
